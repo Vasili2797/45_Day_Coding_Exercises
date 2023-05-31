dc9e747d8864fb5790a8ff4b255dc2c774cd716e
Vasili2797 <nashvilliv1@gmail.com>
2023-05-31 13:41:56 -0400

    ProjectPracticeTests updated

--- projectPracticeTests/src/projectPracticeTests/App.java
+++ projectPracticeTests/src/projectPracticeTests/App.java
@@ -1,5 +1,8 @@
 package projectPracticeTests;
 
 public class App {
-	
+	// this is the place where code that does not work get tested outside of the project
+	public static void main(String[] args) {
+		
+	}
 }
